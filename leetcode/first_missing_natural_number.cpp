71fd62a2f278f23e65773c14481553a4e99259ff
mrig <mriganksagar@gmail.com>
2024-04-20 01:04:12 +0530

    first missing natural number

---
+++ leetcode/first_missing_natural_number.cpp
@@ -0,0 +1,19 @@
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        for(int i = 0; i < nums.size();i++){
+            while(nums[i] != i+1 and nums[i]>0 and nums[i]<=nums.size()){
+                if(nums[nums[i]-1] == nums[i]) break;
+                int x = nums[nums[i]-1];
+                nums[nums[i]-1] = nums[i];
+                nums[i] = x;
+            }
+        }
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] != i+1) return i+1;
+        }
+        return nums.size()+1;
+    }
+};
