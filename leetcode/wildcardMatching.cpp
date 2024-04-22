6c720899c80cb946670ab690f5de250759596e46
mrig <mriganksagar@gmail.com>
2024-04-22 23:59:49 +0530

    wildcard excellent solution without dp

--- leetcode/wildcardMatching.cpp
+++ leetcode/wildcardMatching.cpp
@@ -28,3 +28,54 @@ public:
         return isMatch(s, p, 0, 0, dp);
     }
 };
+
+
+
+
+
+// a very smart solution for this from leetcode by someone really smart
+// the idea is that it backtracks only one level, and still works
+// what is the proof behind it all
+// well the idea is if there are more than one star * in the pattern
+// consider this example  
+// lets say there 
+// s is { s0, s1, s2, s3}
+// p is {p0, * , p1, * , p2 }
+// and s0 s1 matches with p0 * p1
+// but s0 s1 s2 also matches with p0 * p1
+// then why is using the second cases redundant if we are able to get to the next wildcard *
+
+// if we need to match s3 with * p2, and it does as well
+// then we can also match s2 s3 with * p2
+
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int si = 0;
+        int pi = 0;
+        int star_pi = -1;
+        int star_si = -1;
+        while (si < s.size()) {
+            if (pi < p.size() && (p[pi] == '?' || p[pi] == s[si])) {
+                ++si;
+                ++pi;
+            } else if (pi < p.size() && p[pi] == '*') {
+                star_pi = pi++;  // '*' matches empty
+                star_si = si;
+            // no '*' was found or failed to match with the last '*'
+            } else if (star_pi == -1) {
+                return false;
+            } else {
+                pi = star_pi + 1; // backtrack
+                si = ++star_si; // '*' match one more character
+            }
+        }
+
+        // We have reached the end of s. In order to have a match, the rest of characters
+        // in p must be all '*'.
+        for (int i = pi; i < p.size(); ++i) {
+            if (p[i] != '*') return false;
+        }
+        return true;
+    }
+};
