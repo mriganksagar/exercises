9cf16cfea4ae4d9712bcd346b8fa2bf974ef52e2
mrig <mriganksagar@gmail.com>
2024-04-24 15:36:10 +0530

    maxsubarr with element as max

---
+++ leetcode/maxsubarr_with_element_as_max.cpp
@@ -0,0 +1,63 @@
+#include <vector>
+#include <stack>
+#include <unordered_map>
+#include <iostream>
+using namespace std;
+
+// Problem Statement
+// Given an array
+// for every element ai find the maximum length of subarray that would contain this element value as the maximum value
+// return an result array where element ei represent the length for ai
+
+void rightElementWithGreaterValue(const vector<int> &nums, vector<int> &result)
+{
+    stack<int> st;
+    result.clear();
+    result.resize(nums.size());
+    for (int i = nums.size() - 1; i >= 0; i--)
+    {
+        while (!st.empty() and nums[st.top()] < nums[i])
+        {
+            st.pop();
+        }
+        if (st.empty())
+            result[i] = nums.size();
+        else
+            result[i] = st.top();
+        st.push(i);
+    }
+}
+
+vector<int> maxSubArrayWithElementAsMax(const vector<int> &nums)
+{
+    vector<int> rightGreater;
+    // evaluate right greater element index
+    rightElementWithGreaterValue(nums, rightGreater);
+
+    stack<int> st;
+    int leftGreaterIndex;
+
+    vector<int> result;
+    result.resize(nums.size());
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        while (!st.empty() and nums[st.top()] < nums[i])
+        {
+            st.pop();
+        }
+        if (st.empty())
+            leftGreaterIndex = -1;
+        else
+            leftGreaterIndex = st.top();
+        st.push(i);
+        result[i] = rightGreater[i] - leftGreaterIndex - 1;
+    }
+    return result;
+}
+
+int main()
+{
+    auto result = maxSubArrayWithElementAsMax({1,2,1});
+    if(result == vector<int>{1,3,1}) cout<<"well done";
+}
