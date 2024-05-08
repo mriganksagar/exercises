9c26f204e6a70022cb1822b6e4ef0e50cf82e8e0
mrig <mriganksagar@gmail.com>
2024-05-08 15:09:31 +0530

    im strong faster smarter, IM BETTER

--- leetcode/medianOfUniquenessArrary.cpp
+++ leetcode/medianOfUniquenessArrary.cpp
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/find-the-median-of-the-uniqueness-array/
+
 #include<vector>
 #include<unordered_map>
 using namespace std;
@@ -5,21 +7,17 @@ using namespace std;
 class Solution {
 public:
     void umapIncrement(unordered_map<int, int>& umap, int val){
-        // cout<<"umapincr called"<<endl;
         if(umap.find(val) == umap.end()){
             umap[val] =1;
         }
         else umap[val]++;
-        // cout<<"before umap increment return"<<endl;
     }
     void umapDecrement(unordered_map<int, int>& umap, int val){
-        // cout<<"umap decr called"<<endl;
         if(umap.find(val) == umap.end()){
             return;
         }
         else if (umap[val] == 1) umap.erase(val);
         else umap[val]--;
-        // cout<<"before umap decrement return"<<endl;
     }
 
     long long findSubarraysCount(const vector<int>&nums, int x){
@@ -35,7 +33,27 @@ public:
             count+= r-l+1;
             r++;
         }
-        // cout<<"beforereturningsub "<<endl;
+        return count;
+    }
+
+    // this works better because of not using hashmap (faster and more memory efficient) // to my surprise, theres a lot of difference
+    long long findSubarraysCountFaster(const vector<int>&nums, int x){
+        int l = 0, r = 0;
+        int occuranceCount[100001];
+        for(int i = 0; i<=100000; i++){
+            occuranceCount[i] = 0;
+        }
+
+        long long count = 0;
+        int size = 0;
+        while(r < nums.size()){
+            if(occuranceCount[nums[r]]++ == 0) size++;
+            while(size >= x){
+                if(occuranceCount[nums[l++]]-- == 1) size--;
+            }
+            count+= r-l+1;
+            r++;
+        }
         return count;
     }
 
@@ -43,17 +61,12 @@ public:
         int size = nums.size();
         int l = 1, r = size;
         long long countInLeft = ((long long)size*(size+1)/2 -1 )/2;
-        // cout<<"count if left "<<countInLeft<<endl; 
         int answer;
         while(l <= r){
-            // cout<<"l and r "<<l<<" "<<r<<endl; 
             int mid = l + (r-l)/2;
-            // cout<<"mid is "<<mid<<endl;
             long long countSubarrays = findSubarraysCount(nums, mid);
-            // cout<<"coutn sub arrays is "<< countSubarrays<<endl;
             if(countSubarrays <= countInLeft){
                 answer = mid;
-                // cout<<"save answer "<<mid<<endl;
                 l = mid+1;
             }
             else{
