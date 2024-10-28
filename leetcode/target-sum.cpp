389035d0e3a5062d8df88a0ac61968a17b0a0613
mrigank sagar <mriganksagar@gmail.com>
2024-10-28 17:27:52 +0000

    target sum explanation

--- leetcode/target-sum.cpp
+++ leetcode/target-sum.cpp
@@ -1,3 +1,18 @@
+// https://leetcode.com/problems/target-sum/
+// few things to note
+// due to the restraints on nums that sum(nums) <= 1000
+// it was possible to compute this solution 
+// because for a number ni we need target t + ni and t - ni , from previous table,
+// for that target (t` lets say) from previous table we need again t` +- number(i-1) from again previous table
+// which goes on forever
+// but due to limitation that sum of nums < 1000
+// we know that we can never get target > 1000 by any way of adding or subtracting numbers hence i can simply return zero 
+// which i did in the safe dp ( really useful abstraction )
+
+// another important note: 
+// because if target is negative and we want number of ways to acheive that, we can do so by finding -ve of target (which will be positive number)
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
