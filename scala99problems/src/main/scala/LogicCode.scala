76172aa7e4ef685456c1662e808bfce3e6d2088c
mrigank sagar <mriganksagar@gmail.com>
2025-01-19 13:45:21 +0000

    logic questions file

---
+++ scala99problems/src/main/scala/LogicCode.scala
@@ -0,0 +1,17 @@
+
+// P46 (**) Truth tables for logical expressions.
+
+object TruthTableP46 {
+    def and(a: Boolean, b: Boolean): Boolean = a && b
+    def or (a: Boolean, b: Boolean): Boolean = a || b
+    def nand(a: Boolean, b: Boolean): Boolean = !(a && b) 
+    def nor(a: Boolean, b: Boolean): Boolean = !(a||b)
+    def equ(a: Boolean, b : Boolean) = a == b
+    def xor(a: Boolean, b: Boolean): Boolean = !equ(a, b)
+    
+
+}
+
+object LogicCode extends App{
+
+}
