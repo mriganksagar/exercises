2dc4862460e1f80153948620b9fb070d9380d903
mrigank sagar <mriganksagar@gmail.com>
2025-01-21 12:49:18 +0000

    construct all height balanced binary trees v1 height v2 nodes

--- scala99problems/src/main/scala/trees/CreateBTsExercises.scala
+++ scala99problems/src/main/scala/trees/CreateBTsExercises.scala
@@ -4,6 +4,7 @@
     give all possible solutions
  */
 import CreateBTsExercises.constructHeightBalancedBTs
+import scala.annotation.tailrec
 
 object CreateBTsExercises {
     def completelyBalancedBTs[T](v: T)(n: Int): List[Tree[T]] = {
@@ -29,10 +30,10 @@ object CreateBTsExercises {
 
     // P58
     import ShapeBTsExercises._
-    def symmetricCompletelyBalancedBTs[T](v: T)(n: Int): List[Tree[T]] = 
+    def symmetricCompletelyBalancedBTs[T](v: T)(n: Int): List[Tree[T]] =
         completelyBalancedBTs(v)(n).filter(isMirroredStructure)
 
-    /* 
+    /*
         P59: contruct height balanced binary trees
         that means in no tree the height difference between left and right subtree
         should be no greater than 1
@@ -49,33 +50,76 @@ object CreateBTsExercises {
         if h <= 0 then List(EndNode)
         else if h == 1 then List(Node(v))
         else {
-            val subTreesFull = constructHeightBalancedBTs(v)(h-1)
-            val subTreesShort = constructHeightBalancedBTs(v)(h-2)
+            val subTreesFull = constructHeightBalancedBTs(v)(h - 1)
+            val subTreesShort = constructHeightBalancedBTs(v)(h - 2)
             val imperfectHeightBTs = for {
                 subTrees1 <- subTreesFull
                 subTrees2 <- subTreesShort
                 combinedTree <- List(
-                    Node(v, subTrees1, subTrees2),
-                    Node(v, subTrees2, subTrees1)
+                  Node(v, subTrees1, subTrees2),
+                  Node(v, subTrees2, subTrees1)
                 )
             } yield combinedTree
-            
+
             val perfectHeightBTs = for {
                 subTreeLeft <- subTreesFull
                 subTreeRight <- subTreesFull
             } yield Node(v, subTreeLeft, subTreeRight)
 
-            imperfectHeightBTs:::perfectHeightBTs
+            imperfectHeightBTs ::: perfectHeightBTs
         }
 
-    /* 
+    /*
         P60
-        construct height balanced trees with given number of nodes 
-    
+        construct height balanced trees with given number of nodes
+
+        solution =>
+            creating helping functions
+            1. minHBalNodes => that returns minimum possible nodes given a height for height balanced tree
+                as height balanced will have height -1 and height -2 at least in subtrees
+                we call recursively to find min nodes and add them
+
+            2. maxHBalNodes => max possible nodes in given height for hbalBt
+            3. minHeightInHBal => given number of nodes tell min possible height for HbalBt
+                (densely packed but still a height balanced binary tree)
+            4. maxHeightInHBal => given nodes count tell max possible height for HbalBT
+                (sparsely packed tree but satifying height conditions)
+            5. countNodes
      */
-    
+
+    def minNodesInHBal(height: Int): Int =
+        if height <= 0 then 0
+        else if height == 1 then 1
+        else minNodesInHBal(height - 1) + minNodesInHBal(height - 2) + 1
+
+    @tailrec
+    def maxNodeInHBal(height: Int, acc: Int = 1): Int =
+        if height == 0 then acc - 1
+        else maxNodeInHBal(height - 1, acc * 2)
+
+    def minHeightInHBal(nodes: Int): Int =
+        if nodes == 0 then 0
+        else minHeightInHBal(nodes / 2) + 1
+
+    def maxHeightInHBal(nodes: Int): Int =
+        LazyList.from(1).takeWhile(minNodesInHBal(_) <= nodes).last
+
+    def countNodes[T](root: Tree[T]): Int = root match
+        case EndNode => 0
+        case Node(_, leftSt, rightSt) =>
+            1 + countNodes(leftSt) + countNodes(rightSt)
+
+    // it constructs trees given nodes (not height)
+    def constructHeightBalancedBTs_v2[T](v: T)(nodes: Int): List[Tree[T]] =
+        for {
+            _ <- List.empty // just to final container will be list and not sequence
+            height <- minHeightInHBal(nodes) to maxHeightInHBal(nodes)
+            hBalTree <- constructHeightBalancedBTs(v)(height)
+            if hBalTree.countNodes == nodes
+        } yield hBalTree
 }
 object P55Demo extends App {
     // println(CreateBTsExercises.completelyBalancedBTs("x")(4))
-    println(constructHeightBalancedBTs("x")(3))
+    // println(constructHeightBalancedBTs("x")(3))
+
 }
