0dec6b0c300ce22af2cedda00cebb5f08be0dca4
mrigank sagar <mriganksagar@gmail.com>
2025-02-23 15:57:31 +0530

    layout binary tree 3

--- scala99problems/src/main/scala/homegrownDatastructures/Tree.scala
+++ scala99problems/src/main/scala/homegrownDatastructures/Tree.scala
@@ -3,77 +3,110 @@ package homegrownDatastructures
 // A representation of Tree and specific type of trees using inhertence and OOP
 // My own implementation , (might have some issues too)
 
-sealed trait Tree[+T]{
+sealed trait Tree[+T] {
     // common tree methods
 }
 
 sealed trait PositionedTree[+T] extends Tree[T] {
     // methods specific to PositionedTree
 }
-sealed trait BoundedTree[+T] extends Tree[T] {
+
+sealed trait BoundedTree[+T](val bounds: List[(Int, Int)]) extends Tree[T] {
     // methods specific to BoundedTree
 }
 
-case class Node[+T](
-    value: T,
-    left: Tree[T] = End,
-    right: Tree[T] = End
-) extends Tree[T] {
-    // methods common for node
-}
+// case class Node[+T](
+//     value: T,
+//     left: Tree[T] = EndNode,
+//     right: Tree[T] = EndNode
+// ) extends Tree[T] {
+//     // methods common for node
+// }
 
-case object End extends EndNode
+/* 
+    creating it so that left of positioned node can be positioned node instead of just node
+    however i had to remove +T covariant because of extending it in PositionedNode and bounding node
+    it was giving error that it appears in contravariant position i.e when Node instantiation 
+ */
+
+case class Node[U <: Tree, T](
+    value: T,
+    left: U[T] = EndNode,
+    right: U[T] = EndNode
+) extends Tree[T] {
+    // common methods
+}
 
 sealed trait EndNode extends Tree[Nothing] {
     // all common methods
 }
 
-class PositionedNode[+T](
+case object EndNode extends EndNode
+
+class PositionedNode[T](
     v: T,
     l: PositionedTree[T],
     r: PositionedTree[T],
     val x: Int,
     val y: Int
 ) extends Node(v, l, r)
-    with PositionedTree[T]
+    with PositionedTree[T] {
+    override def toString(): String =
+        s"PN(${super.toString()}, x is ${this.x} and y is ${this.y}})"
+}
 
-object PositionedNode{
+object PositionedNode {
     def unapply[T](v: PositionedNode[T]) =
         Some(Node(v.value, v.left, v.right), v.x, v.y)
 }
 
 case object PositionedEnd extends EndNode with PositionedTree[Nothing] {
     // methods for positionedEnd
+    override def toString() = "[PE]"
 }
 
-class BoundedNode[+T](
+class BoundedNode[T](
     v: T,
     l: BoundedTree[T],
     r: BoundedTree[T],
-    val bounds: List[(Int, Int)]
+    bounds: List[(Int, Int)],
+    val edgeLength: Int
 ) extends Node(v, l, r)
-    with BoundedTree[T]
+    with BoundedTree[T](bounds)
 
-case object BoundedEnd extends EndNode with BoundedTree[Nothing]{
+object BoundedNode {
+    def unapply[T](v: BoundedNode[T]) =
+        Some(v.value, v.left, v.right, v.bounds, v.edgeLength)
+}
+
+case object BoundedEnd extends EndNode with BoundedTree[Nothing](Nil) {
     // methods for boundedEnd
 }
 
 object deemo extends App {
-    val v = PositionedNode(4, PositionedNode(1, PositionedEnd, PositionedEnd, 0,0), PositionedEnd, 1,1)
+    val v = PositionedNode(
+      4,
+      PositionedNode(1, PositionedEnd, PositionedEnd, 0, 0),
+      PositionedEnd,
+      1,
+      1
+    )
 
     println(v.getClass())
-    println(v.isInstanceOf[PositionedTree[Int]])
-    println(v.isInstanceOf[Node[Int]])
-    println(End.isInstanceOf[Tree[Int]])
+    println(v.isInstanceOf[PositionedTree[Int]]) // true
+    println(v.isInstanceOf[Node[Tree, Int]])    // true
+    println(EndNode.isInstanceOf[Tree[Int]])   // true
 
-    val x : Tree[Int] = Node(1, End, End)
+    val x: Tree[Int] = Node(1, EndNode, EndNode)
 
+
+    // exhaustive matching works well in case of simple Simple Trees
     x match
         case Node(value, left, right) => println("it is a node")
-        case e:EndNode => println("endnode")
+        case e: EndNode               => println("endnode")
 
-    
+    // pattern matching unapply works well
     v match
-        case PositionedNode(Node(value, left, right), x, y) => println(s" $left,;;; $right")
-    
+        case PositionedNode(Node(value, left, right), x, y) =>
+            println(s" $left, $right")
 }
--- scala99problems/src/main/scala/root/trees/Layout3BinaryTree.scala
+++ scala99problems/src/main/scala/root/trees/Layout3BinaryTree.scala
@@ -1,13 +1,64 @@
 package root.trees
 
 import homegrownDatastructures.*
+
 /*
+ P66 (***) Layout a binary tree (3).
  pack the layout tightly but with sufficient space between nodes in same level
+
+ soln: 
+    I need to know how much horizonal span left and right subtree takes
+    and need to distance required between those subtrees, after that i can know
+    how much should be the edge length for the root that will add those left right subtrees
+    then find x-offset for the root and recursively give subtrees their position based on 
+    edge length of root
  */
 
-def createBoundedTree[T](root: Tree[T]): BoundedTree[T] = ???
+def createBoundedTree[T](root: Tree[T]): BoundedTree[T] = root match
+    case Node(value, left, right) => {
+        val leftBT = createBoundedTree(left)
+        val rightBT = createBoundedTree(right)
+        // it is the needed horizontal distance between left and right subtrees
+        val edgeL = leftBT.bounds.view
+            .zip(rightBT.bounds)
+            .map((a, b) => a._2 - b._1 + 1)
+            .maxOption
+            .map {
+                case n if n % 2 == 1 => (n + 1) / 2
+                case n               => n / 2
+            }
+            .getOrElse(1)
 
+        val rootBounds = (0,0) :: leftBT.bounds.view
+            .map((a, b) => Some(a - edgeL, b - edgeL))
+            .zipAll(
+              rightBT.bounds.map((a, b) => Some(a + edgeL, b + edgeL)),
+              None,
+              None
+            )
+            .collect {
+                case (Some(l1, l2), Some(r1, r2)) => (l1, r2)
+                case (None, Some(r))              => r
+                case (Some(l), None)              => l
+            }.toList
+        
+        BoundedNode(value, leftBT, rightBT, rootBounds, edgeL)
+    }
+    case _: EndNode => BoundedEnd
+
+
+def layout3aux[T](root:BoundedTree[T], y: Int, x: Int): PositionedTree[T] = root match
+    case BoundedNode[T](v, l, r, b, e) => PositionedNode(v, layout3aux(l, y+1, x-e), layout3aux(r, y+1, x+e), x, y)
+    case BoundedEnd => PositionedEnd
+
+def layout3[T](root: Tree[T]): PositionedTree[T] = {
+    val boundedTree = createBoundedTree(root)
+    val rootXoffset = boundedTree.bounds.map(-_._1).max
+
+    layout3aux(boundedTree, 0, rootXoffset)
+}
 
 object Layout3Demo extends App {
-
+    val result = layout3(Node('a', Node('b', EndNode, Node('c')), Node('d')))
+    println(result.toString())
 }
