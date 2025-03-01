354be12997f9ac34ec2acf46ae0f97a26ae454d1
mrigank sagar <mriganksagar@gmail.com>
2025-03-01 15:37:46 +0530

    multiway tree questions

--- scala99problems/src/main/scala/homegrownDatastructures/MultiwayTrees.scala
+++ scala99problems/src/main/scala/homegrownDatastructures/MultiwayTrees.scala
@@ -1,10 +1,24 @@
 package homegrownDatastructures
 
+import scala.annotation.tailrec
+
 // P70 (**) Tree construction from a node string
 case class MTree[+T](value: T, children: List[MTree[T]] = Nil){
     override def toString(): String = s"$value {${children.mkString(",")}}"
 
     def countNodes: Int = 1 + children.map(_.countNodes).sum
+
+
+    // returns pair - (internalpathlength, nodescount)
+    private def internalPathLengthAux: (Int, Int) = 
+        children.view.map(_.internalPathLengthAux).map(p => (p._1 + p._2 ) -> p._2).fold((0, 1))((a, b) => (a._1 + b._1, a._2 + b._2))       
+    
+
+    def internalPathLength: Int = this.internalPathLengthAux._1
+
+    def postOrder: List[T] = {
+        children.map(_.postOrder).foldRight(Nil)(_:::_).appended(value)
+    }
 }
 
 object MTree{
@@ -17,11 +31,23 @@ object MTree{
                 (firstChild:: rest._1) -> rest._2 
             }
         }
-        val rest = mSubtrees(s.drop(1))
+
+        @tailrec
+        def mSubtreesTail(s: String, acc: List[MTree[Char]] = Nil): (List[MTree[Char]], String) = {
+            if s(0) == '^' then acc.reverse -> s.drop(1)
+            else {
+                val (child, restString) = createMTreeAux(s)
+                mSubtreesTail(restString, child::acc)
+            }
+        }
+
+        val rest = mSubtreesTail(s.drop(1))
         MTree(s(0), rest._1) -> rest._2
     }
 
     def createMTree(s:String): MTree[Char] = createMTreeAux(s)._1
+
+    given stringToMTree: Conversion[String, MTree[Char]] = s => createMTree(s)
 }
 
 object DemoMTree extends App {
@@ -29,6 +55,12 @@ object DemoMTree extends App {
     println(a.countNodes)
     println(a)
 
+    val mtree1 = MTree.createMTree("afg^^c^bd^e^^^")
+    println(mtree1.toString())
+    println(mtree1.internalPathLength)
 
-    println(MTree.createMTree("afg^^c^bd^e^^^").toString())
+    import MTree.given
+    println(
+        "afg^^c^bd^e^^^".postOrder
+    )
 }
