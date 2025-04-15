bb52cab4785a2927eeee0a2d98d2c75247774068
Mr.Mrigank <mriganksagar@gmail.com>
2025-04-15 23:13:02 +0530

    djikstra (maybe) immutable implementation

--- scala99problems/dsa/src/main/scala/graphs/BFSShortestPath.scala
+++ scala99problems/dsa/src/main/scala/graphs/BFSShortestPath.scala
@@ -1,4 +1,5 @@
 // given a station and its neighbouring station
+import scala.collection.immutable.TreeMap
 
 
 object GraphBFS {
@@ -16,5 +17,31 @@ object GraphBFS {
       }
     aux(List(List(start)), target, Set(start)).map(_.reverse)
   }  
+  
+  // I am considering positive weights
+  case class Edge(weight: Int, to: Int)
+  type Graph = Map[Int, List[Edge]] // first is
+  case class Path(cost: Int, path: List[Int]) // weight and Path
+  def bfsShortestPathWeightedG(graph: Graph, start: Int, target: Int): Option[List[Int]] = {
+    given Ordering[(Int, Path)] with {
+      def compare(x: (Int, Path), y: (Int, Path)): Int = x._2.cost - y._2.cost
+    }
+      
+    def aux(st: TreeMap[Int, Path], visited: Set[Int]): Option[Path] = st.minOption
+      .flatMap{
+        case (to, path) if to == target => Some(path)
+        case (to, path) => {
+          val neighbors = graph.getOrElse(to, Nil).filterNot(edge => visited(edge.to))
+          val newSt = neighbors.foldLeft(st.removed(to)){
+              (_st, edge) => _st.updatedWith(edge.to){
+                  case s @ Some(Path(cost, _)) if cost <= edge.weight + path.cost => s
+                  case _ => Some(Path(path.cost + edge.weight, edge.to::path.path))
+                }
+            }  
+          aux(newSt, visited + to)
+        }
+      }
+    aux(TreeMap(start -> Path(0, List(start))), Set.empty).map(_.path.reverse)
+  }
 }
 
