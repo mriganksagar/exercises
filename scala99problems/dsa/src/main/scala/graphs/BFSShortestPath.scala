7bd81772296c1661f62b3ff4cf16023b55281be3
Mr.Mrigank <mriganksagar@gmail.com>
2025-04-15 23:39:19 +0530

    refactor djikstra

--- scala99problems/dsa/src/main/scala/graphs/BFSShortestPath.scala
+++ scala99problems/dsa/src/main/scala/graphs/BFSShortestPath.scala
@@ -20,25 +20,24 @@ object GraphBFS {
   
   // I am considering positive weights
   case class Edge(weight: Int, to: Int)
-  type Graph = Map[Int, List[Edge]] // first is
-  case class Path(cost: Int, path: List[Int]) // weight and Path
+  type Graph = Map[Int, List[Edge]] 
+  case class Path(cost: Int, path: List[Int])
   def bfsShortestPathWeightedG(graph: Graph, start: Int, target: Int): Option[List[Int]] = {
     given Ordering[(Int, Path)] with {
       def compare(x: (Int, Path), y: (Int, Path)): Int = x._2.cost - y._2.cost
     }
       
-    def aux(st: TreeMap[Int, Path], visited: Set[Int]): Option[Path] = st.minOption
-      .flatMap{
+    def aux(paths: TreeMap[Int, Path], visited: Set[Int]): Option[Path] = paths.minOption.flatMap{
         case (to, path) if to == target => Some(path)
         case (to, path) => {
-          val neighbors = graph.getOrElse(to, Nil).filterNot(edge => visited(edge.to))
-          val newSt = neighbors.foldLeft(st.removed(to)){
-              (_st, edge) => _st.updatedWith(edge.to){
+          val unvisitedNeighbors = graph.getOrElse(to, Nil).filterNot(edge => visited(edge.to))
+          val newPaths = unvisitedNeighbors.foldLeft(paths.removed(to)){
+              (_paths, edge) => _paths.updatedWith(edge.to){
                   case s @ Some(Path(cost, _)) if cost <= edge.weight + path.cost => s
                   case _ => Some(Path(path.cost + edge.weight, edge.to::path.path))
                 }
             }  
-          aux(newSt, visited + to)
+          aux(newPaths, visited + to)
         }
       }
     aux(TreeMap(start -> Path(0, List(start))), Set.empty).map(_.path.reverse)
